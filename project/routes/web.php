f4f0d63125dbd7fe6fd14a4e1e2fb0984ea7f87e
Jami <jamijoyy@gmail.com>
2020-05-10 16:11:04 +0600

    Feature Update: Route Reconfigured

--- project/routes/web.php
+++ project/routes/web.php
@@ -1,20 +1,13 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 Route::get('/logout', 'GeneralControllers\LogoutController@index');
 
 Route::get('/', 'GeneralControllers\LoginController@index');
 Route::post('/', 'GeneralControllers\LoginController@verify');
 
-Route::get('/home', 'GeneralControllers\GeneralHomeController@index')->name('home.index');
+Route::group(['middleware'=>['IdentityVerifier']], function(){
+	
+	Route::get('/home', 'GeneralControllers\GeneralHomeController@index')->name('home.index');
 
-Route::get('/home/postManager', 'PostManagerControllers\PostManagerHomeController@index')->name('postManagerHome.index');
+	Route::get('/home/postManager', 'PostManagerControllers\PostManagerHomeController@index')->name('postManagerHome.index');
+});
